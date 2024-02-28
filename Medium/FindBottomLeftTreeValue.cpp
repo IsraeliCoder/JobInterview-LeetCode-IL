722039bdb8e26bed9d8f8391143fda35ba6909f5
orleg01 <orleg01@gmail.com>
2024-02-28 12:48:13 +0200

    Longest Sub-String No Repeating

--- Medium/FindBottomLeftTreeValue.cpp
+++ Medium/FindBottomLeftTreeValue.cpp
@@ -36,6 +36,20 @@ public:
 
     int findBottomLeftValue(TreeNode* root) {
         
+		/*
+		
+		Given the root of a binary tree, return the leftmost value in the last row of the tree.
+		
+		Example 1:
+		Input: root = [2,1,3]
+		Output: 1
+		
+		Example 2:
+		Input: root = [1,2,3,4,null,5,6,null,null,7]
+		Output: 7
+		
+		*/
+		
         int answer = 0;
         int rank = 0;
         int maxRank = 0;
